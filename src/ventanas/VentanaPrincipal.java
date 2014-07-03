091997117bbfeed54dc22572a43753b719cf1cf1
Triperito <nahuelsarubbio@hotmail.com>
2014-07-03 18:07:49 -0300

    No toque nada 2

--- src/ventanas/VentanaPrincipal.java
+++ src/ventanas/VentanaPrincipal.java
@@ -25,8 +25,6 @@ public class VentanaPrincipal extends JFrame {
 	private JPanel contentPane;
 	public static JTextField textField;
 	public static JTextArea txt;
-	public static JTextArea txt2;
-
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
