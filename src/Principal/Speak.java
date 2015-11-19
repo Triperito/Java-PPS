2000f75d2d84957e23e40afcebca3d35895374cf
Alan <Alan@Alan-PC>
2015-11-19 18:36:24 -0300

    Cambio

--- src/Principal/Speak.java
+++ src/Principal/Speak.java
@@ -27,7 +27,7 @@ public class Speak {
 
 	public static void eSpeak(String txt){
 		try {
-			Runtime.getRuntime().exec(pathAbsoluto + "\\eSpeak\\command_line\\espeak -v" + voz + " \"" + txt + "\"");
+			Runtime.getRuntime().exec(pathAbsoluto + "eSpeak\\command_line\\espeak -v" + voz + " \"" + txt + "\"");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -35,7 +35,7 @@ public class Speak {
 	
 	public static void eSpeakFile(String file){
 		try {
-			Runtime.getRuntime().exec(pathAbsoluto + "\\eSpeak\\command_line\\espeak -v" + voz + " -f "+file);
+			Runtime.getRuntime().exec(pathAbsoluto + "eSpeak\\command_line\\espeak -v" + voz + " -f "+file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
