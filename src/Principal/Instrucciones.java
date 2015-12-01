1e2dba8e679b86a8998bfbc62f1244122550d048
Nahuel <Nahuel@Nahuel-NPC>
2015-12-01 16:03:57 -0300

    Todo ok!

--- src/Principal/Instrucciones.java
+++ src/Principal/Instrucciones.java
@@ -63,10 +63,12 @@ public class Instrucciones extends JFrame {
 		contentPane.add(lblTitulo, BorderLayout.NORTH);
 		
 		JTextPane textPaneInstrucciones = new JTextPane();
+		textPaneInstrucciones.setFont(new Font("Arial", Font.PLAIN, 12));
+		textPaneInstrucciones.setDisabledTextColor(Color.BLACK);
 		textPaneInstrucciones.setFocusable(false);
 		textPaneInstrucciones.setEditable(false);
 		textPaneInstrucciones.setAutoscrolls(true);
-		textPaneInstrucciones.setText("Ventana de Configuracion:\r\n");
+		textPaneInstrucciones.setText("----------Ventana Configuraci\u00F3n----------\r\n\u2022 Si es la priemra vez que utiliza la aplicaci\u00F3n debera seguir las intrucciones paso a paso de como instalar el motor de voz de la aplicaci\u00F3n, para esto haga click en el bot\u00F3n Manual de instalaci\u00F3n y le abrira un PDF con dichas instrucciones.\r\n\u2022 Podra elegir una voz que sera la que se reproduzca en la aplicaci\u00F3n, mediante el bot\u00F3n azul podra escucharlas preliminarmente.\r\n\u2022 Activar sonido de botones: Genera un sonido cada vez que se presiona una letra o espacio en la aplicaci\u00F3n.\r\n\u2022 Activar guardado de texto: Dara la posibilidad de guardar lo escrito hasta el momento en un archivo de texto en la carpeta \"Textos\".\r\n\u2022 Bot\u00F3n Reproducir archivo: Dara la posibilidad de elegir un archivo previamente guardado para su reproducci\u00F3n.\r\n\u2022 Bot\u00F3n Manual de Instalci\u00F3n: Abrira un PDF con las intrucciones paso a paso de como instalar el motor de voz de la aplicaci\u00F3n. \r\n\r\n----------En la Aplicaci\u00F3n----------\r\n\u2022 Dispondra de grupos de letras a los cuales debera introducirse para escoger la letra correspondiente, si se equivoca de grupo, podra volver atras con el bot\u00F3n espacio (Bot\u00F3n violeta).\r\n\u2022 Si se equivoca y es necesario borrar puede precionar el bot\u00F3n \"Borrar\" para eliminar una letra, o mantenerlo precionado para borrar grupos de letras\".\r\n\u2022 Al presionar el bot\u00F3n \"Punto\" se terminara una oraci\u00F3n, la misma sera reproducida con la voz elegida.");
 		//for(int i=1; i<cadenas.length; i++) {
 		//	textPaneInstrucciones.setText(textPaneInstrucciones.getText() + cadenas[i]+"\n");
 		//}
